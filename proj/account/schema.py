60ad32f8427ce45ca8b290cb9249ee626fb0a6a6
Qi Zhao <zhaoqi99@outlook.com>
2022-06-05 23:03:23 +0800

    feat: 🎸 try relay

--- proj/account/schema.py
+++ proj/account/schema.py
@@ -1,7 +1,8 @@
 import graphene
+from graphene import relay
 from graphene_django.types import DjangoObjectType
 
-from account.models import User,Token
+from account.models import Token, User
 
 
 class UserType(DjangoObjectType):
@@ -13,14 +14,21 @@ class UserType(DjangoObjectType):
     class Meta:
         model = User
         convert_choices_to_enum = False
+        interfaces = (relay.Node,)
+
+
+class UserConnection(relay.Connection):
+    class Meta:
+        node = UserType
+
 
 class UserQuery(graphene.ObjectType):
-    users = graphene.List(UserType)
+    users = relay.ConnectionField(UserConnection)
     user_by_name = graphene.Field(UserType, user_name=graphene.String())
 
     def resolve_users(self, info, **kwargs):
         return User.objects.all()
-    
+
     def resolve_user_by_name(self, info, user_name):
         return User.objects.filter(username=user_name).first()
 
@@ -29,11 +37,13 @@ class TokenType(DjangoObjectType):
     class Meta:
         model = Token
 
+
 class TokenQuery(graphene.ObjectType):
     token_by_value = graphene.Field(TokenType, value=graphene.String())
 
     def resolve_token_by_value(self, info, value):
         return Token.objects.filter(token=value).first()
 
-class Query(UserQuery,TokenQuery):
+
+class Query(UserQuery, TokenQuery):
     pass
